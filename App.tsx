962ab10dcca6d7cac7a8ee471f25c2e6953646bf
Alexs1313 <o.salata@o-salata.local>
2025-08-28 14:47:04 +0300

    upd

--- App.tsx
+++ App.tsx
@@ -1,7 +1,74 @@
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeLoader from './src/components/WelcomeLoader';
+import { PartoContextProvider } from './src/store/context';
+import { useEffect, useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import PartoDiaryOfMemoriesScreen from './src/screens/PartoDiaryOfMemoriesScreen';
+import CreatePartoMemoryScreen from './src/screens/CreatePartoMemoryScreen';
+import MemoryPartoCardDetails from './src/screens/MemoryPartoCardDetails';
+import RecommendedPartoScreen from './src/screens/RecommendedPartoScreen';
+import RecommendedPartoList from './src/screens/RecommendedPartoList';
+import PartoPlacesCardDetails from './src/screens/PartoPlacesCardDetails';
+import SavedPartoList from './src/screens/SavedPartoList';
+import PartoMapScreen from './src/screens/PartoMapScreen';
+import PartoMainScreen from './src/screens/PartoMainScreen';
+import EntryScreen from './src/screens/EntryScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <View></View>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(true);
+    }, 5000);
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <PartoContextProvider>
+        {isLoading ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="EntryScreen" component={EntryScreen} />
+            <Stack.Screen name="PartoMainScreen" component={PartoMainScreen} />
+            <Stack.Screen
+              name="PartoDiaryOfMemoriesScreen"
+              component={PartoDiaryOfMemoriesScreen}
+            />
+            <Stack.Screen
+              name="CreatePartoMemoryScreen"
+              component={CreatePartoMemoryScreen}
+            />
+            <Stack.Screen
+              name="MemoryPartoCardDetails"
+              component={MemoryPartoCardDetails}
+            />
+            <Stack.Screen
+              name="RecommendedPartoScreen"
+              component={RecommendedPartoScreen}
+            />
+
+            <Stack.Screen
+              name="RecommendedPartoList"
+              component={RecommendedPartoList}
+            />
+
+            <Stack.Screen
+              name="PartoPlacesCardDetails"
+              component={PartoPlacesCardDetails}
+            />
+
+            <Stack.Screen name="SavedPartoList" component={SavedPartoList} />
+
+            <Stack.Screen name="PartoMapScreen" component={PartoMapScreen} />
+          </Stack.Navigator>
+        ) : (
+          <WelcomeLoader />
+        )}
+      </PartoContextProvider>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/components/WelcomeLoader.js
@@ -0,0 +1,43 @@
+import LottieView from 'lottie-react-native';
+import { Dimensions, Image, StyleSheet, View } from 'react-native';
+import MainBackground from '../components/MainBackground';
+const { height } = Dimensions.get('window');
+
+const WelcomeLoader = () => {
+  return (
+    <MainBackground>
+      <View style={styles.container}>
+        <LottieView
+          source={require('../assets/animations/confetti.json')}
+          autoPlay
+          style={{ width: 550, height: 470 }}
+        />
+
+        <Image
+          source={require('../assets/images/appLogo.png')}
+          style={styles.logo}
+        />
+
+        <View style={{ alignItems: 'center' }}>
+          <Image
+            source={require('../assets/images/loaderImg.png')}
+            style={{}}
+          />
+          <LottieView
+            source={require('../assets/animations/pulsing.json')}
+            autoPlay
+            style={styles.spinner}
+          />
+        </View>
+      </View>
+    </MainBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', marginTop: height * 0.05 },
+  logo: { position: 'absolute', top: 90 },
+  spinner: { width: 380, height: 230, position: 'absolute', top: 190 },
+});
+
+export default WelcomeLoader;
